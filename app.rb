25efb1d929f4bfc441e47298cd50432725ddc29d
Blen Wondimagegn <mekuriablen@gmail.com>
2020-10-05 08:01:20 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,24 @@
 require_relative 'config/environment'
+require_relative "models/piglatinizer.rb"
 
 class App < Sinatra::Base
+  
+  get '/' do 
+    erb :user_input 
+  end 
+  
+  # post '/piglatinize' do
+  #   @user_phrase = params[:user_text]
+  #   erb :piglatinize
+  # end 
+  post '/piglatinize' do
+    @user_input = params["user_phrase"]
+    @p = Piglatinizer.new 
+    erb :piglatinize
+  end
 end
+
+# <% pl_init = PigLatinizer.new %>
+
+# <h2>Thing: <%= pl_init.piglatinize(@user_phrase) %></h2>
+end 
